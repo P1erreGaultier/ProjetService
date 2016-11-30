e295b7f67733da591800f4490f43e96c52154fc4
P1erreGaultier <pit.gaultier@free.fr>
2016-11-30 22:59:58 +0100

    SOAP working & refactor infra

---
+++ com.alma.fournisseur.infrastructure/src/main/java/com/alma/wsimport/ValidateCard.java
@@ -0,0 +1,64 @@
+
+package com.alma.wsimport;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour anonymous complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="card" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "card"
+})
+@XmlRootElement(name = "validateCard")
+public class ValidateCard {
+
+    @XmlElement(required = true)
+    protected String card;
+
+    /**
+     * Obtient la valeur de la propriété card.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCard() {
+        return card;
+    }
+
+    /**
+     * Définit la valeur de la propriété card.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCard(String value) {
+        this.card = value;
+    }
+
+}
